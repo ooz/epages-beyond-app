4cae69ea731637f6b1b0b74195434cd0c2f901ee
Mathias Düsterhöft <mduesterhoeft@epages.com>
2018-07-06 13:57:02 +0200

    Remove access token global variable

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Author: Oliver Zscheyge
 Description:
     Web app that generates beautiful order documents for ePages shops.
-'''
+"""
 
 import os
-import sys
 
 import epages
 from flask import Flask, render_template, request, Response, abort, escape
@@ -26,7 +25,6 @@ app = Flask(__name__)
 CLIENT_ID = ''
 CLIENT_SECRET = ''
 API_URL = ''
-ACCESS_TOKEN = ''
 CLIENT = None
 ORDER_DB = {}
 ORDERS_FOR_MERCHANT_KEY = ''
@@ -34,18 +32,17 @@ ORDERS_FOR_MERCHANT_KEY = ''
 
 @app.route('/')
 def root():
-    if has_private_app_credentials() or has_byd_credentials():
+    if has_byd_credentials():
         return render_template('index.html', installed=True)
     return render_template('index.html', installed=False)
-
+ 
 @app.route('/callback')
 def callback():
-    global ACCESS_TOKEN
     global API_URL
     args = request.args
-    ACCESS_TOKEN, API_URL, return_url = epages.get_access_token(CLIENT_ID, CLIENT_SECRET, args)
+    access_token, API_URL, return_url = epages.get_access_token(CLIENT_ID, CLIENT_SECRET, args)
     init_client()
-    print('access_token: %s' % ACCESS_TOKEN)
+    print('access_token: %s' % access_token)
     return """<!DOCTYPE html>
 <html>
 <head>
@@ -74,7 +71,7 @@ u'''<h1>Something went wrong when fetching the order list! :(</h1>
 <pre>
 %s
 </pre>
-''' % escape(unicode(e)), 400
+''' % escape(str(e)), 400
 
 # Requires wkhtmltox or wkhtmltopdf installed besides Python's pdfkit
 @app.route('/api/pdfs/<order_id>.pdf')
@@ -96,12 +93,12 @@ def pdf(order_id):
 
 @app.before_request
 def limit_open_proxy_requests():
-    '''Security measure to prevent:
+    """Security measure to prevent:
     http://serverfault.com/questions/530867/baidu-in-nginx-access-log
     http://security.stackexchange.com/questions/41078/url-from-another-domain-in-my-access-log
     http://serverfault.com/questions/115827/why-does-apache-log-requests-to-get-http-www-google-com-with-code-200
     http://stackoverflow.com/questions/22251038/how-to-limit-flask-dev-server-to-only-one-visiting-ip-address
-    '''
+    """
     if not is_allowed_request():
         print("Someone is messing with us:")
         print(request.url_root)
@@ -124,35 +121,30 @@ def init():
     global CLIENT_ID
     global CLIENT_SECRET
     global API_URL
-    global ACCESS_TOKEN
     global CLIENT
     global ORDER_DB
 
     CLIENT_ID = os.environ.get('CLIENT_ID', '')
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET', '')
     API_URL = os.environ.get('API_URL', '')
-    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', '')
 
     init_client()
 
     ORDER_DB = {}
 
     assert has_client_credentials_or_private_app_credentials(), \
-        'Please set either CLIENT_ID and CLIENT_SECRET or API_URL and ACCESS_TOKEN as environment variables!'
+        'Please set either CLIENT_ID and CLIENT_SECRET and API_URL as environment variables!'
 
 def init_client():
     global CLIENT
     global ORDERS_FOR_MERCHANT_KEY
     CLIENT = epages.BYDClient(API_URL, CLIENT_ID, CLIENT_SECRET)
-    ORDERS_FOR_MERCHANT_KEY = API_URL + ACCESS_TOKEN
+    ORDERS_FOR_MERCHANT_KEY = API_URL
 
 def has_client_credentials_or_private_app_credentials():
-    return has_client_credentials() or \
-           has_private_app_credentials()
+    return has_client_credentials()
 def has_client_credentials():
     return CLIENT_ID != '' and CLIENT_SECRET != ''
-def has_private_app_credentials():
-    return API_URL != '' and ACCESS_TOKEN != ''
 def has_byd_credentials():
     return API_URL != '' and has_client_credentials()
 
